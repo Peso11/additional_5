d08504ed7df01b41aa5728c1270fac66b8140fe2
Peso11 <answer0333@gmail.com>
2018-02-27 01:54:21 +0300

    rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  var stek = [];
+  str = str.split('');
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && stek[stek.length - 1] == bracketsConfig[j][0]) {
+        stek.pop();
+      } else if (str[i] === bracketsConfig[j][0]) {
+        stek.push(str[i]);
+      }
+    }
+  }
+
+  if (stek.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
